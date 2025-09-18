644a34451b80d701fcedacbd4d75fd5dcf1eeba0
John M. Drescher <drescherjm@gmail.com>
2025-09-18 18:22:54 -0400

    Update some documentation!

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# simple_vcpkg_library_application
-This repository creates a test application that links to the simple_vcpkg_library which is setup to be built using vcpkg in manifest mode.
+# simple\_vcpkg\_library\_application
+
+This repository creates a test application that links to the [simple\_vcpkg\_library](https://github.com/drescherjm/simple_vcpkg_library) which is setup to be built using vcpkg in manifest mode.
+
+# Project Design Notes
+
+1. vcpkg.json is used to setup the dependency to the library created in the simple\_vcpkg\_library.
+```json
+   {
+     "name": "simple-vcpkg-library-application",
+     "version-string": "0.1.0",
+     "dependencies": [
+       "simple-vcpkg-library"
+     ]
+   }
+```
+2. vcpkg-configuration.json is used to specify where the local port overlay that contains the portfiles needed for the library.
+```
+{
+  "overlay-ports": [
+    "vcpkg/ports"
+  ],
+  "overlay-triplets": [
+    "vcpkg/triplets"
+  ]
+}
+```
+Although I defined a location for overlay-triplets it's not currently used in this example.
--- vcpkg/ports/simple-vcpkg-library/portfile.cmake
+++ vcpkg/ports/simple-vcpkg-library/portfile.cmake
@@ -1,10 +1,12 @@
-set(GIT_REF ad16a2e0ccaaaef0d06a9cb79bb16df8fe936c97)
+set(GIT_REF_of_library 1d050ed372fdb0374a272965a55375d0a4a0a13f)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO drescherjm/simple_vcpkg_library
-    REF ${GIT_REF} # commit or tag
-    SHA512 ad5757f62b95b967625370bd098adb3d0a07b3e92f1ec803ae005c5e0a98cd2ffb6658fac18fb3cd3dd10ef951e5031652b35b8ec1cfafe4d03155bca4ae54f1
+    REF ${GIT_REF_of_library} 
+    SHA512 0d528823cd6cbaafa786bc68aacd650edf945fc1c28fdcbc7a570b598ae8e87993cce3573a0ffeae28a5d15cefccf0e160ef51a209d674cfe4179faa278a4d94
+	#Note: The SHA512 will need to change if you change the GIT_REF_of_library. The next CMake configure will fail and tell you
+	#the new SHA512 code. Copy and paste the note: Actual  : SHA512 hash code above.
 )
 
 vcpkg_cmake_configure(
@@ -14,7 +16,10 @@ vcpkg_cmake_configure(
 vcpkg_cmake_build()
 vcpkg_cmake_install()
 
-# Fixup cmake config locations and paths
+# Fixup cmake config locations and paths. This is essential for vcpkg to setup the SimpleLibraryExampleConfig.cmake file 
+# and setup each configuration for multiconfig generators like Visual Studio 2022. 
+# Remember that vcpkg builds the Debug and Release config then maps the other configs: MinSizeRel, RelWithDebInfo to
+# use the binaries created for the Release config. 
 vcpkg_cmake_config_fixup(PACKAGE_NAME SimpleLibraryExample CONFIG_PATH lib/cmake/SimpleLibraryExample)
 
 # Install license into share/${PORT}/
